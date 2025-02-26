be73c9a2e4a3dc4e929248ae2db558d094a4be7d
sonudahiya123 <er.sonudahiya@gmail.com>
2025-02-26 17:28:52 +0530

    node test code

--- server.js
+++ server.js
@@ -12,8 +12,7 @@ app.get('/api/get_data',(req,res)=>{
      const workbook = xlsx.readFile(filepath);
      const sheet_name_list =workbook.SheetNames;
      const data = xlsx.utils.sheet_to_json(workbook.Sheets[sheet_name_list[0]]);
-     res.json(data);
-           
+     res.json(data);           
 }).listen(port,(error)=>{
        if(!error)
          console.log(`Server is started on http://localhost:${port} successfully.`);
